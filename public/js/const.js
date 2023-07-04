4df6ff13962cb9e4deb7d2c45ceee6a8c4ecbd5f
Jami infante <jami@MacBook.lan>
2023-07-04 23:02:27 +0200

    Update graphique

--- public/js/const.js
+++ public/js/const.js
@@ -1,16 +1,15 @@
 
-const ZERO = "0️⃣";
-const UN = "1️⃣";
-const DEUX = "2️⃣";
-const TROIS = "3️⃣";
-const QUATRE = "4️⃣";
-const CINQ = "5️⃣";
-const SIX = "6️⃣";
-const SEPT = "7️⃣";
-const HUIT = "8️⃣";
-const NEUF = "9️⃣";
+const ZERO = "0";
+const UN = "1️";
+const DEUX = "2️";
+const TROIS = "3️";
+const QUATRE = "4️";
+const CINQ = "5️";
+const SIX = "6️";
+const SEPT = "7️";
+const HUIT = "8️";
+const NEUF = "9️";
 const DRAPEAU = "🚩";
 const EXPLOSION = "💥";
 const BOMBE = "💣";
 const CARRE_NOIR = "⬛";
-//source https://emojiterra.com/fr/
